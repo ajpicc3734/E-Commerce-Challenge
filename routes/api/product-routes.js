477af64232636fea4a6d4107dff1fef066ca0f3a
ajpicc3734 <ajpicc3734@gmail.com>
2022-06-04 09:06:16 -0400

    created product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -19,6 +19,15 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+
+  Product.findOne({
+    include: [Category, { model: Tag, through: ProductTag }],
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbProductData) => res.json(dbProductData))
+    .catch((err) => res.json(err));
 });
 
 // create new product
